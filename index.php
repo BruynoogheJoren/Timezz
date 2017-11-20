b5ab2a803e668011e55d1d47fbfdd16be5aedb16
jorenbruynooghe <joren.bruynooghe@student.howest.be>
2017-11-20 08:53:47 +0100

    geenidee

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Timezz</title>
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans+Narrow" rel="stylesheet">
+    <link rel="stylesheet" href="css/screen.css">
+</head>
+<body>
+<header>
+    <h1>TIMEZZ</h1>
+</header>
+<main>
+    <section id="container">
+        <article id="screen">
+           tadaaaaaaaaaa
+        </article>
+        <article id="phone">
+            <img src="images/Iphone.png" height="895" width="441" alt="iphone">
+        </article>
+    </section>
+</main>
+<footer>
+</footer>
+</body>
+</html>
